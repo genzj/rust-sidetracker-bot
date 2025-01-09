cdddb60ed191c856dcac0f503ce9ecee814dc9a8
genzj <zj0512@gmail.com>
2025-01-09 10:35:44 -0800

    feat: :sparkles: migrate atprotol client lib to the atrium-api

--- src/api.rs
+++ src/api.rs
@@ -1,103 +1,98 @@
 use crate::util::{dump_to_private_file, is_file_exists, load_from_file};
-use atproto_api::{Agent, AtpAgent};
-use futures::FutureExt;
-use log::{debug, info, trace};
-use serde_json::{json, Value};
+use atrium_api::agent::{store::MemorySessionStore, AtpAgent, Session};
+use atrium_api::app::bsky::feed::defs::ThreadViewPost;
+use atrium_api::app::bsky::feed::get_post_thread::{OutputThreadRefs, ParametersData};
+use atrium_api::types::Union;
+use atrium_xrpc_client::reqwest::ReqwestClient;
+use log::{info, trace};
 use std::env;
-use std::panic::AssertUnwindSafe;
+use std::error::Error;
+use std::ops::Deref;
 
 const SESSION_FILE: &str = "session.json";
 
-pub async fn must_create_agent() -> Result<AtpAgent, Box<dyn std::error::Error>> {
-    let mut agent: Option<AtpAgent> = None;
+type BskyClient = AtpAgent<MemorySessionStore, ReqwestClient>;
+
+fn new_client(base_url: &str) -> BskyClient {
+    AtpAgent::new(ReqwestClient::new(base_url), MemorySessionStore::default())
+}
+
+pub async fn must_create_agent() -> Result<BskyClient, Box<dyn std::error::Error>> {
+    let client = new_client("https://bsky.social");
+    // TODO delegate to SessionStore
     if is_file_exists(SESSION_FILE).await {
-        debug!("loading from session file {:?}", SESSION_FILE);
-        let mut session_agent = AtpAgent::new("https://bsky.social".to_string());
-        match load_from_file(SESSION_FILE).await {
-            Ok(session) => {
-                debug!("session file loaded successfully");
-                session_agent.session = Some(session);
-                let refresh_result = AssertUnwindSafe(async {
-                    debug!("trying refreshing session");
-                    session_agent.clone().refresh_session().await
-                })
-                    .catch_unwind()
-                    .await;
-                match refresh_result {
-                    Ok(Ok(())) => {
-                        info!("saved session refreshed successfully");
-                        agent = Some(session_agent)
-                    }
-                    Ok(Err(err)) => {
-                        debug!("session refresh failed: {:?}", err);
-                    }
-                    Err(err) => {
-                        debug!("session refresh panic: {:?}", err);
-                    }
-                }
-            }
-            Err(err) => {
-                debug!("session load failed: {:?}", err);
+        if let Ok(session) = load_from_file::<Session>(SESSION_FILE).await {
+            if let Ok(_) = client.resume_session(session).await {
+                return Ok(client);
             }
         }
     }
-
-    if agent.is_none() {
-        info!("resuming session failed, fallback to login");
-        let login_agent = AtpAgent::new("https://bsky.social".to_string())
-            .login(
-                env::var("BLUESKY_IDENTIFIER").unwrap(),
-                env::var("BLUESKY_PASSWORD").unwrap(),
-            )
-            .await?;
-        info!("logged in successfully");
-        trace!("logged in as {:?}", &login_agent.session);
-        debug!("dumping session to file {SESSION_FILE}");
-        dump_to_private_file(SESSION_FILE, &login_agent.session).await?;
-        agent = Some(login_agent);
-    }
-    Ok(agent.unwrap())
+    client
+        .login(
+            env::var("BLUESKY_IDENTIFIER").unwrap(),
+            env::var("BLUESKY_PASSWORD").unwrap(),
+        )
+        .await?;
+    let _ = dump_to_private_file(SESSION_FILE, &client.get_session().await.unwrap()).await;
+    Ok(client)
 }
 
 pub async fn get_post_thread(
-    agent: AtpAgent,
+    client: BskyClient,
     uri: String,
-) -> Result<Value, Box<dyn std::error::Error>> {
-    let params = json!({
-      "uri": uri,
-      "depth": "1"
-    });
-
-    let res = agent
-        .get("app.bsky.feed.getPostThread".to_string(), params)
+) -> Result<ThreadViewPost, Box<dyn Error>> {
+    let res = client
+        .api
+        .app
+        .bsky
+        .feed
+        .get_post_thread(
+            ParametersData {
+                depth: Some(1u16.try_into().unwrap()),
+                parent_height: Some(20.try_into().unwrap()),
+                uri: uri,
+            }
+            .into(),
+        )
         .await?;
 
-    Ok(res)
+    match &res.thread {
+        Union::Refs(OutputThreadRefs::AppBskyFeedDefsThreadViewPost(post)) => {
+            trace!("downloaded post: {:?}", post);
+            return Ok(post.deref().clone());
+        }
+        _ => {
+            info!("post: {:?}", res);
+        }
+    }
+    Err("not found".into())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::util::ensure_tailing_slash;
-    use atproto_api::Session;
+    use atrium_api::agent::Session;
     use mockito::Matcher::PartialJsonString;
     use mockito::{Matcher, Server};
 
     fn create_test_session() -> Session {
         let session = r#"{
             "accessJwt": "test-saved-access-jwt",
-            "did": "test-did",
-            "handle": "test-handle",
+            "did": "did:plc:test_did",
+            "handle": "test.handle",
             "refreshJwt": "test-saved-refresh-jwt"
         }"#;
         serde_json::from_str::<Session>(session).unwrap()
     }
 
-    fn create_test_agent(server: &Server) -> AtpAgent {
-        let url = ensure_tailing_slash(&server.url());
-        let mut agent = AtpAgent::new(url);
-        agent.session = Some(create_test_session());
-        agent
+    async fn create_test_agent(server: &Server) -> BskyClient {
+        let url = &server.url();
+        let url = url.strip_suffix('/').unwrap_or(url);
+        let client = new_client(&url);
+        let resume = client.resume_session(create_test_session()).await;
+        info!("resume: {:?}", resume);
+        assert!(resume.is_ok());
+        client
     }
 
     async fn mock_create_session(server: &mut Server) -> &mut Server {
@@ -107,14 +102,37 @@ mod tests {
             .match_body(PartialJsonString(
                 r#"{"identifier":"handle", "password":"password"}"#.to_string(),
             ))
-            .match_header("User-Agent", "atproto_api/0.1.0")
             .with_status(200)
+            .with_header("content-type", "application/json")
             .with_body(
                 r#"{
                     "accessJwt": "test-logged-access-jwt",
-                    "did": "test-did",
-                    "handle": "test-handle",
+                    "did": "did:plc:test_did",
+                    "handle": "test.handle",
                     "refreshJwt": "test-logged-refresh-jwt",
+                }"#,
+            )
+            .create_async()
+            .await;
+        server
+    }
+
+    async fn mock_get_session(server: &mut Server) -> &mut Server {
+        server
+            .mock("GET", "/xrpc/com.atproto.server.getSession")
+            .match_header(
+                "authorization",
+                Matcher::AnyOf(vec![
+                    Matcher::Exact("Bearer test-logged-access-jwt".to_string()),
+                    Matcher::Exact("Bearer test-saved-access-jwt".to_string()),
+                ]),
+            )
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(
+                r#"{
+                    "did": "did:plc:test_did",
+                    "handle": "test.handle"
                 }"#,
             )
             .create_async()
@@ -132,13 +150,13 @@ mod tests {
                     Matcher::Exact("Bearer test-saved-refresh-jwt".to_string()),
                 ]),
             )
-            .match_header("User-Agent", "atproto_api/0.1.0")
             .with_status(200)
+            .with_header("content-type", "application/json")
             .with_body(
                 r#"{
                     "accessJwt": "test-refreshed-access-jwt",
-                    "did": "test-did",
-                    "handle": "test-handle",
+                    "did": "did:plc:test_did",
+                    "handle": "test.handle",
                     "refreshJwt": "test-refreshed-refresh-jwt",
                 }"#,
             )
@@ -152,20 +170,22 @@ mod tests {
     async fn mock_get_post_thread(server: &mut Server) -> &mut Server {
         server
             .mock("GET", "/xrpc/app.bsky.feed.getPostThread")
-            .match_header("User-Agent", "atproto_api/0.1.0")
             .match_header(
                 "authorization",
                 Matcher::AnyOf(vec![
                     Matcher::Exact("Bearer test-logged-access-jwt".to_string()),
                     Matcher::Exact("Bearer test-saved-access-jwt".to_string()),
                     Matcher::Exact("Bearer test-refreshed-access-jwt".to_string()),
+                    Matcher::Missing,
                 ]),
             )
             .match_query(Matcher::AllOf(vec![
-                Matcher::UrlEncoded("uri".to_string(), TEST_THREAD_URI.to_string()),
                 Matcher::UrlEncoded("depth".to_string(), "1".to_string()),
+                Matcher::UrlEncoded("parentHeight".to_string(), "20".to_string()),
+                Matcher::UrlEncoded("uri".to_string(), TEST_THREAD_URI.to_string()),
             ]))
             .with_status(200)
+            .with_header("content-type", "application/json")
             .with_body_from_file("test_data/thread_3leb44umzuc2l.json5")
             .create_async()
             .await;
@@ -175,14 +195,16 @@ mod tests {
     #[tokio::test]
     async fn test_get_post_thread() {
         let mut server = Server::new_async().await;
+        mock_get_session(&mut server).await;
         mock_refresh_session(&mut server).await;
         mock_get_post_thread(&mut server).await;
-        let agent = create_test_agent(&server);
-        let res = get_post_thread(agent, TEST_THREAD_URI.to_string())
-            .await
-            .unwrap();
-        assert!(res.is_object());
-        assert_eq!(res["thread"]["$type"], "app.bsky.feed.defs#threadViewPost");
+        let agent = create_test_agent(&server).await;
+        let res = get_post_thread(agent, TEST_THREAD_URI.to_string()).await;
+        assert!(res.is_ok());
+        assert_eq!(
+            res.unwrap().post.uri,
+            "at://did:plc:xn5b64qpivpq55wumwf6wdjg/app.bsky.feed.post/3leb44umzuc2l"
+        );
     }
 
     #[tokio::test]
--- src/post.rs
+++ src/post.rs
@@ -1,5 +1,9 @@
+use atrium_api::app::bsky::embed::record::ViewRecordRefs;
+use atrium_api::app::bsky::feed::defs::{PostView, ThreadViewPost};
+use atrium_api::app::bsky::feed::defs::{PostViewEmbedRefs, ThreadViewPostParentRefs};
+use atrium_api::app::bsky::feed::post::RecordData;
+use atrium_api::types::{TryFromUnknown, Union, Unknown};
 use log::debug;
-use serde_json::Value;
 use std::collections::VecDeque;
 use url::Url;
 
@@ -92,38 +96,59 @@ impl Post {
     }
 }
 
-pub fn parse_post_text(post: &Value) -> &str {
-    post["record"]["text"].as_str().unwrap().trim()
+pub fn parse_record_from_unknown(unknown: &Unknown) -> Option<RecordData> {
+    if let Ok(record) = TryFromUnknown::try_from_unknown(unknown.clone()) {
+        Some(record)
+    } else {
+        None
+    }
 }
 
-pub fn parse_post_uri(post: &Value) -> &str {
-    post["uri"].as_str().unwrap()
+pub fn parse_post_text(post: &PostView) -> String {
+    if let Some(record) = parse_record_from_unknown(&post.record) {
+        record.text.trim().to_string()
+    } else {
+        String::new()
+    }
 }
 
-pub fn parse_post_author_handle(post: &Value) -> &str {
-    post["author"]["handle"].as_str().unwrap()
+pub fn parse_post_uri(post: &PostView) -> String {
+    post.uri.clone()
 }
 
-pub fn parse_embedded(post: &Value) -> Option<Post> {
-    let value = &post["post"]["embed"]["record"];
-    let ret = match (
-        value["value"]["text"].as_str(),
-        value["author"]["handle"].as_str(),
-        value["uri"].as_str(),
-    ) {
-        (Some(text), Some(handle), Some(uri)) if text.trim() != "" => {
-            Some(Post::new(handle, text.trim(), uri, 0))
+pub fn parse_post_author_handle(post: &PostView) -> String {
+    post.author.handle.to_string()
+}
+
+pub fn get_parent<'a>(thread: &'a ThreadViewPost) -> Option<&'a ThreadViewPost> {
+    if let Some(Union::Refs(ThreadViewPostParentRefs::ThreadViewPost(k))) = &thread.parent {
+        Some(k)
+    } else {
+        None
+    }
+}
+
+pub fn parse_embedded(post: &Option<Union<PostViewEmbedRefs>>) -> Option<Post> {
+    if let &Some(Union::Refs(PostViewEmbedRefs::AppBskyEmbedRecordView(ref box_view))) = post {
+        if let &Union::Refs(ViewRecordRefs::ViewRecord(ref box_record)) = &box_view.record {
+            if let Some(record) = parse_record_from_unknown(&box_record.value) {
+                return Some(Post::new(
+                    box_record.author.handle.as_str(),
+                    record.text,
+                    box_record.uri.as_str(),
+                    0,
+                ));
+            }
         }
-        (_, _, _) => None,
-    };
-    ret
+    }
+    None
 }
 
-pub fn flatten_thread(thread_envelope: &Value) -> VecDeque<Post> {
-    let mut thread = &thread_envelope["thread"];
+pub fn flatten_thread(thread_view_post: &ThreadViewPost) -> VecDeque<Post> {
     let mut result = VecDeque::with_capacity(10);
+    let mut cur: &ThreadViewPost = thread_view_post;
     loop {
-        let post = &thread["post"];
+        let post = &cur.post;
         let post = Post::new(
             parse_post_author_handle(post),
             parse_post_text(post),
@@ -134,13 +159,14 @@ pub fn flatten_thread(thread_envelope: &Value) -> VecDeque<Post> {
         if post.text.len() > 0 {
             result.push_front(post)
         }
-        if thread["parent"].is_null() {
-            if let Some(p) = parse_embedded(&thread) {
-                result.push_front(p)
+        if cur.parent.is_none() {
+            if let Some(post) = parse_embedded(&cur.post.embed) {
+                result.push_front(post);
             }
             break;
+        } else if let Some(k) = get_parent(cur) {
+            cur = k;
         }
-        thread = &thread["parent"];
     }
 
     // renumber the posts
@@ -156,24 +182,41 @@ pub fn flatten_thread(thread_envelope: &Value) -> VecDeque<Post> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use atrium_api::app::bsky::feed::get_post_thread;
+    use crate::post::tests::TestPost::{LeafPostThread, RootPostThread};
 
-    fn thread() -> Value {
-        let data: Value =
-            serde_json::from_str(include_str!("../test_data/thread_3leb44umzuc2l.json5")).unwrap();
-        data["thread"].to_owned()
+    enum TestPost {
+        LeafPostThread,
+        RootPostThread,
+    }
+
+    fn load_test_thread(test_post: TestPost) -> ThreadViewPost {
+        let test_file = match test_post {
+            TestPost::LeafPostThread => "test_data/thread_3leb44umzuc2l.json5",
+            TestPost::RootPostThread => "test_data/thread_3lfd7fhrkyk24.json5",
+        };
+        let output: get_post_thread::Output =
+            serde_json5::from_slice(&std::fs::read(test_file).unwrap()).unwrap();
+        if let Union::Refs(get_post_thread::OutputThreadRefs::AppBskyFeedDefsThreadViewPost(post)) =
+            &output.thread
+        {
+            *post.to_owned()
+        } else {
+            panic!("unexpected test data");
+        }
     }
 
     #[test]
     fn test_parse_post_text() {
-        let thread = thread();
-        let text = parse_post_text(&thread["post"]);
+        let thread = load_test_thread(LeafPostThread);
+        let text = parse_post_text(&thread.post);
         assert_eq!(text, "猛吃！");
     }
 
     #[test]
     fn test_parse_post_uri() {
-        let thread = thread();
-        let uri = parse_post_uri(&thread["post"]);
+        let thread = load_test_thread(LeafPostThread);
+        let uri = parse_post_uri(&thread.post);
         assert_eq!(
             uri,
             "at://did:plc:xn5b64qpivpq55wumwf6wdjg/app.bsky.feed.post/3leb44umzuc2l"
@@ -182,23 +225,39 @@ mod tests {
 
     #[test]
     fn test_parse_post_author_handle() {
-        let thread = thread();
-        let handle = parse_post_author_handle(&thread["post"]);
+        let thread = load_test_thread(LeafPostThread);
+        let handle = parse_post_author_handle(&thread.post);
         assert_eq!(handle, "nghua.me");
     }
 
     #[test]
+    fn test_get_parent() {
+        let thread = load_test_thread(LeafPostThread);
+        let parent = get_parent(&thread);
+        assert!(parent.is_some());
+        let uri = parse_post_uri(&parent.unwrap().post);
+        assert_eq!(
+            uri,
+            "at://did:plc:7tf4afounuzjqioojiwln3jv/app.bsky.feed.post/3leb3s4oc222d"
+        );
+    }
+
+    #[test]
+    fn test_get_parent_return_none() {
+        let thread = load_test_thread(RootPostThread);
+        let parent = get_parent(&thread);
+        assert!(parent.is_none());
+    }
+
+    #[test]
     fn test_parse_embedded() {
-        //.thread.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent
-        let thread = thread();
-        let mut level = &thread;
-        loop {
-            if let Value::Null = level["parent"] {
-                break;
-            }
-            level = &level["parent"];
+        let thread = load_test_thread(LeafPostThread);
+        let mut level = Some(&thread);
+        while let cur @ Some(_) = get_parent(level.unwrap()) {
+            level = cur;
         }
-        let embedded = parse_embedded(level).unwrap();
+
+        let embedded = parse_embedded(&level.unwrap().post.embed).unwrap();
         assert_eq!(embedded.handle, "cotranedolphy.bsky.social");
         assert!(embedded.text.starts_with("以前有个同事"));
         assert_eq!(
@@ -214,20 +273,27 @@ mod tests {
 
     #[test]
     fn test_parse_embed_return_none() {
-        let thread = thread();
-        let embedded = parse_embedded(&thread);
+        let thread = load_test_thread(LeafPostThread);
+        let embedded = parse_embedded(&thread.post.embed);
         assert!(embedded.is_none());
     }
 
     #[test]
     fn test_flatten_thread() {
-        let thread_envelope =
-            serde_json::from_str(include_str!("../test_data/thread_3leb44umzuc2l.json5")).unwrap();
-
-        let flattened = flatten_thread(&thread_envelope);
+        let thread = load_test_thread(LeafPostThread);
+        let flattened = flatten_thread(&thread);
         assert_eq!(flattened.len(), 13);
-        assert_eq!(flattened[0].uri, "at://did:plc:fkjudld5cg4ailkuyec65wvg/app.bsky.feed.post/3le73kidz7k2e");
-        assert_eq!(flattened[1].uri, "at://did:plc:xn5b64qpivpq55wumwf6wdjg/app.bsky.feed.post/3le7txyg4y22e");
-        assert_eq!(flattened.back().unwrap().uri, "at://did:plc:xn5b64qpivpq55wumwf6wdjg/app.bsky.feed.post/3leb44umzuc2l");
+        assert_eq!(
+            flattened[0].uri,
+            "at://did:plc:fkjudld5cg4ailkuyec65wvg/app.bsky.feed.post/3le73kidz7k2e"
+        );
+        assert_eq!(
+            flattened[1].uri,
+            "at://did:plc:xn5b64qpivpq55wumwf6wdjg/app.bsky.feed.post/3le7txyg4y22e"
+        );
+        assert_eq!(
+            flattened.back().unwrap().uri,
+            "at://did:plc:xn5b64qpivpq55wumwf6wdjg/app.bsky.feed.post/3leb44umzuc2l"
+        );
     }
 }
